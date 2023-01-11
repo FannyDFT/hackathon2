2bfc9dc43ae51511c3647aecd98f993691851bf8
FannyDFT <fannyd.erfurth@yahoo.fr>
2023-01-11 19:06:22 +0100

    Add Caroussel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "@next/font/google";
 import FindACar from "../src/components/FindACar";
+import MiniCaroussel from "../src/components/MiniCaroussel";
 import Navbar from "../src/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,6 +11,7 @@ export default function Home() {
       <Navbar />
       <div>Let&apos; Go Hackathon</div>
       <FindACar />
+      <MiniCaroussel />
     </div>
   );
 }

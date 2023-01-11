e0eb36e2b902e94752616c6b0c6c08b1b9c86e0d
e-genie <eugenie.froustey@gmail.com>
2023-01-11 14:38:29 +0100

    added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      Quicksand: ["Quicksand", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],

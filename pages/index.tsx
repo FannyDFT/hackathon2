4383c410d37f98a7c3e6f1749a7a04d1c3729e37
e-genie <eugenie.froustey@gmail.com>
2023-01-11 16:54:26 +0100

    Revert "Booking form"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
 import { Inter } from "@next/font/google";
-import BookingForm from "../src/components/BookingForm";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div>
-      Let&apos; Go Hackathon
-      <BookingForm />
-    </div>
-  );
+  return <div>Let&apos; Go Hackathon</div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,10 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        Quicksand: ["Quicksand", "sans-serif"],
-      },
-      colors: {
-        blueColor: "#26498D",
-        grayColor: "#D9D9D9",
-        textGray: "#6F6C6C",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };

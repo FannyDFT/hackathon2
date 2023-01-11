5a9d8f8433b8b2915c35712906fd13f2234a35ce
FannyDFT <fannyd.erfurth@yahoo.fr>
2023-01-11 11:32:18 +0100

    Add Prisma

---
+++ pages/api/cars/index.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../prisma/client";
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {};
+
+export default handler;

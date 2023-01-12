46765cc95f710711813420d314215cacf9f04fdc
FannyDFT <fannyd.erfurth@yahoo.fr>
2023-01-12 11:13:45 +0100

    Add Link For Booking one car

--- src/components/CardCar.tsx
+++ src/components/CardCar.tsx
@@ -1,16 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 function CardCar() {
   return (
     <div className="flex justify-around border w-5/6 items-center rounded-lg">
-      <div className="flex flex-col">
-        <div className="text-xl font-bold">car model</div>
-        <div>or similar</div>
-        <div className="text-xl font-semibold">price € / day </div>
-      </div>
-      <div className="flex">
-        <div>image</div>
-      </div>
+      <Link href="/detailscars">
+        <div className="flex flex-col">
+          <div className="text-xl font-bold">car model</div>
+          <div>or similar</div>
+          <div className="text-xl font-semibold">price € / day </div>
+        </div>
+        <div className="flex">
+          <div>image</div>
+        </div>
+      </Link>
     </div>
   );
 }

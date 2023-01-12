9d97b24f3b8bde1ceb6d5513a4e28036c0ce6b4e
MaximeB29 <belin.maxime9@gmail.com>
2023-01-12 12:22:21 +0100

    cloudinary first

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    loader: "cloudinary",
+    path: "http://res.cloudinary.com/dqylyotdo/fleetgo/upload",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
